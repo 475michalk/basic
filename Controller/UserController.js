2f47d1b264acd00e7ae4206fd30f107fea2fdf8f
475michal <166406013+475michalk@users.noreply.github.com>
2024-07-03 15:27:26 +0300

    valid

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,14 +1,30 @@
-exports.createUser = () => {
-    console.log("addUser function");
-}
-exports.updateUser = () => {
-    console.log("updateUser function");
+exports.createUser = (req, res) => {
+   
+        const { id, nameUser, email, phone } = req.body;
+        const newUser = User.createUser(id, nameUser, email, phone);
+        res.status(201).json(newUser);
+    }
 
-}
-exports.deleteUser = () => {
-    console.log("deleteUser function");
-}
+exports.updateUser = (req, res) => {
+        const { id } = req.params;
+        const updatedUser = User.updateUser(Number(id), req.body);
+        if (updatedUser) {
+            res.json(updatedUser);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    }
 
+
+exports.deleteUser = (req,res) => {
+    const { id } = req.params;
+    const deletedUser = User.deleteUser(Number(id));
+    if (deletedUser) {
+        res.json(deletedUser);
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+}
 
 
 
